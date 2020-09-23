8c59250d792e3f035918d9cc1c913b72093b9f33
Shawn P. Conroy <shawn@snowbank.ca>
2020-09-23 14:59:21 -0400

    more verbose update

--- controllers/UpdateController.php
+++ controllers/UpdateController.php
@@ -5,18 +5,18 @@ class UpdateController
 {
     public static function updateDictionaryData($config, $verbose)
     {
-        if ($verbose) echo "<p>Updating dictionary data...</p>";
+        if ($verbose) echo "<h2>📖 Updating dictionary data...</h2>";
         if ($verbose) echo "<p> ... download word list.</p>";
         downloadFile($config->remoteCsvLocation, $config->csvLocation);
         $rawWords = loadCsv($config->csvLocation);
-        if ($verbose) echo "<p>Processing words...</p>";
+        if ($verbose) echo "<p> ... Processing words</p>";
         $dictionary = Word::createDictionary($config, $rawWords, $verbose);
         Word::saveDictionary($config, $dictionary);
     }
 
     public static function updateLanguageData($config, $verbose)
     {
-        if ($verbose) echo "<p>Downloading i18n language data...</p>";
+        if ($verbose) echo "<h2>🌐 Downloading i18n language data...</h2>";
         $langResourceCSV = fopen($config->remoteI18nCsvLocation, 'r');
         if ($langResourceCSV === false) {
             die("Failed to open lang CSV");
@@ -24,11 +24,13 @@ class UpdateController
         //What does this do on failure? Empty file? No file found?
 
         $columnNames = fgetcsv($langResourceCSV);
+        if ($verbose) echo "<p> ... found columns for ".implode(", ", $columnNames)."</p>";
 
         while (($textData = fgetcsv($langResourceCSV)) !== false) {
             foreach ($textData as $key=>$datum) {
                 if ($key == 0) {
                     $textId = $datum;
+                    if ($verbose) echo $datum.' ';
                     continue;
                 }
                 $langResource[$columnNames[$key]][$textId] = $datum;
--- update.php
+++ update.php
@@ -5,12 +5,13 @@ include_once 'bootstrap.php';
 // If not run by client show status
 $verbose = (php_sapi_name()!=="cli");
 if ($verbose) {
-    echo "<html><head><title>".$config->siteName." Update</title></head><body>";
+    echo "<html><head><title>".$config->siteName." Update</title></head><body>
+    <h1>Updating local files</h1>";
 }
 
 UpdateController::updateDictionaryData($config, $verbose);
 UpdateController::updateLanguageData($config, $verbose);
 
 if ($verbose) {
-    echo "</body></html>";
+    echo "<h2>👍 Updating complete.</h2></body></html>";
 }
