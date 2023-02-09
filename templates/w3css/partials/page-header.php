64f8056fd5c475174763703362ac1ebb600161b3
ShawnPConroy <shawn@snowbank.ca>
2023-02-09 11:44:03 -0500

    Fix link

--- templates/w3css/partials/page-header.php
+++ templates/w3css/partials/page-header.php
@@ -11,7 +11,7 @@ namespace WorldlangDict;
         <a href="<?php echo WorldlangDictUtils::makeUri($config, '', $request); ?>"><?php echo $config->getTrans('random word button');?></a> &bull;
         <a href="https://xwexi.globasa.net/<?php echo $config->lang;?>/gramati/lexiklase"><?php echo $config->getTrans('word classes link');?></a> &bull;
         <a href="<?php echo WorldlangDictUtils::makeUri($config, 'tul/translation-aide', $request); ?>"><?php echo $config->getTrans('translation aide title');?></a> &bull;
-        <a href="<?php echo WorldlangDictUtils::makeUri($config, 'tul/abeceli-menalari', $request); ?>"><?= $config->getTrans('browse title') ?></a> &bull;
+        <a href="<?php echo WorldlangDictUtils::makeUri($config, 'abeceli-menalari', $request); ?>"><?= $config->getTrans('browse title') ?></a> &bull;
         
         <a href="<?php echo WorldlangDictUtils::makeUri($config, 'lexilari', $request); ?>"><?php echo $config->getTrans('all words button');?></a> &bull;
         <a href="<?php echo WorldlangDictUtils::makeUri($config, 'tul', $request); ?>"><?php echo $config->getTrans('tools button');?></a>
