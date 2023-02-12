e71d8b8a6180373eb0979e45cf9665cf39d17f5b
ShawnPConroy <shawn@snowbank.ca>
2023-02-11 22:24:31 -0500

    url fix

--- templates/w3css/partials/page-header.php
+++ templates/w3css/partials/page-header.php
@@ -10,7 +10,7 @@ namespace WorldlangDict;
     </h1>
         <a href="<?php echo WorldlangDictUtils::makeUri($config, 'lexi', $request); ?>"><?php echo $config->getTrans('random word button');?></a> &bull;
         <a href="https://xwexi.globasa.net/<?php echo $config->lang;?>/gramati/lexiklase"><?php echo $config->getTrans('word classes link');?></a> &bull;
-        <a href="<?php echo WorldlangDictUtils::makeUri($config, 'tul/translation-aide', $request); ?>"><?php echo $config->getTrans('translation aide title');?></a> &bull;
+        <a href="<?php echo WorldlangDictUtils::makeUri($config, 'tul/trasbasatul', $request); ?>"><?php echo $config->getTrans('translation aide title');?></a> &bull;
         <a href="<?php echo WorldlangDictUtils::makeUri($config, 'abeceli-menalari', $request); ?>"><?= $config->getTrans('browse title') ?></a> &bull;
         
         <a href="<?php echo WorldlangDictUtils::makeUri($config, 'lexilari', $request); ?>"><?php echo $config->getTrans('all words button');?></a> &bull;
