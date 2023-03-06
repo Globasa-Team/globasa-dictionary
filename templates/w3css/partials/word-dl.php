aa7aee1c29f27342547981bcde58f22685b39fce
shawn@snowbank.ca <shawn@snowbank.ca>
2023-03-06 15:16:28 -0500

    :bug: tags: fix term lookup with capital letters

--- templates/w3css/partials/word-dl.php
+++ templates/w3css/partials/word-dl.php
@@ -1,8 +1,10 @@
 <? namespace WorldlangDict; ?>
 <dl class="dictionaryList tags_extended">
 <?php
-foreach($list as $cur):
-    $word = $config->dictionary->words[$cur];
+foreach($list as $termIndex=>$term):
+    if (!isset($config->dictionary->words[$termIndex])) continue;
+    $word = $config->dictionary->words[$termIndex];
+    
     $letter = ctype_alpha($word->termIndex[0]) ? $word->termIndex[0] : $word->termIndex[1];
     ?>
     <dt data-class="<?= $word->wordClass; ?>" data-category="<?= $word->category; ?>" data-char="<?= $letter; ?>"><?= WorldlangDictUtils::makeLink(
