7c9b98e6551033db1ea7d83acdd6b07e8fdf8cfb
shawn@snowbank.ca <shawn@snowbank.ca>
2023-03-07 13:48:10 -0500

    :construction: better error log

--- WorldlangDictUtils.php
+++ WorldlangDictUtils.php
@@ -6,7 +6,16 @@ class WorldlangDictUtils
     public static function makeUri($config, $controller, $request)
     {
         if (is_string($request)) {
-            error_log("-\n\nmakeUri has a request as string.\n\n".serialize($request)."\n\n".serialize(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS)), 3, "debug.log");
+            error_log("\n\n-----".date(DATE_ATOM)."\n", 3, "debug.log");
+            error_log("\nmakeUri has a request as string.\n", 3, "debug.log");
+            error_log("\nrequest:\n".serialize($request)."\n", 3, "debug.log");
+            error_log("\nbacktrace:\n".serialize(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS))."\n", 3, "debug.log");
+    
+            foreach(debug_backtrace() as $trace) {
+                
+                error_log("\nfile :".$trace['file']."\n", 3, "debug.log");
+                error_log("\nfile :".$trace['class'].$trace['type'].$trace['function'].$trace['line']."\n", 3, "debug.log");
+            }
         }
         return $config->siteUri.$config->lang.'/'.$controller.(!is_string($request) ? $request->linkQuery : "");
     }
