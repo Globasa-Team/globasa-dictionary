8a92ef6894cf7645e4d9d0c0922ae24b59d4264c
shawn@snowbank.ca <shawn@snowbank.ca>
2023-03-10 09:54:19 -0500

    :construction: more debug tracing

--- WorldlangDictUtils.php
+++ WorldlangDictUtils.php
@@ -11,6 +11,16 @@ class WorldlangDictUtils
             error_log("\nrequest:\n".serialize($request)."\n", 3, "debug.log");
             error_log("\nbacktrace:\n".serialize(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS))."\n", 3, "debug.log");
     
+
+            foreach($_SERVER as $key=>$cur) {
+                if (!in_array($key, ["UNIQUE_ID", "SCRIPT_URI", "REQUEST_URI", "SCRIPT_NAME", "REDIRECT_UNIQUE_ID", "REDIRECT_SCRIPT_URL", "REDIRECT_SCRIPT_URI", "REDIRECT_URL", "REDIRECT_QUERY_STRING"]) continue;
+                if (is_string($cur)){
+                    error_log("\n_SERVER[{$key}]: ".serialize($cur)."\n", 3, "debug.log");
+                } else {
+                    error_log("\n_SERVER[{$key}]: ".$cur."\n", 3, "debug.log");
+                }
+            }
+
             foreach(debug_backtrace() as $trace) {
                 
                 error_log("\nfile :".$trace['file']."\n", 3, "debug.log");
@@ -37,10 +47,34 @@ class WorldlangDictUtils
 
     public static function redirect($config, $request, $controller="")
     {
-        error_log("\n\n-----".date(DATE_ATOM)."\n", 3, "debug.log");
-        error_log("\nredirect on request:\n".serialize($request)."\n", 3, "debug.log");
+    
+        if (is_string($request)) {
+            
+            error_log("\n\n-----".date(DATE_ATOM)."\n", 3, "debug.log");
+            error_log("\nredirect on request:\n".serialize($request)."\n", 3, "debug.log");
+            error_log("\n_SERVER: ".$_SERVER["REQUEST_URI"]."\n", 3, "debug.log");
+            error_log("\n_SERVER: ".$_SERVER["SCRIPT_NAME"]."\n", 3, "debug.log");
+            error_log("\n_SERVER: ".$_SERVER["PHP_SELF"]."\n", 3, "debug.log");
+            error_log("\n_SERVER: ".$_SERVER["SCRIPT_URI"]."\n", 3, "debug.log");
+            foreach($_REQUEST as $key=>$cur) {
+                if (is_string($cur)){
+                    error_log("\n_REQUEST[{$key}]: ".serialize($cur)."\n", 3, "debug.log");
+                } else {
+                    error_log("\n_REQUEST[{$key}]: ".$cur."\n", 3, "debug.log");
+                }
+            }
+            
+            foreach($_SERVER as $key=>$cur) {
+                if (is_string($cur)){
+                    error_log("\n_SERVER[{$key}]: ".serialize($cur)."\n", 3, "debug.log");
+                } else {
+                    error_log("\n_SERVER[{$key}]: ".$cur."\n", 3, "debug.log");
+                }
+            }
+        }
+
         header('Location: '.WorldlangDictUtils::makeUri($config, $controller, $request));
-        die();
+        exit(0);
     }
 
     public static function makeLink($config, $controller, $request, $text=null)
