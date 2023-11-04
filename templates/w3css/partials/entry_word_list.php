979129c1c5270fbd0b5eec8617b57667c6f11928
shawn@snowbank.ca <shawn@snowbank.ca>
2023-11-04 21:03:20 +0000

    make entry lists clickable

--- templates/w3css/partials/entry_word_list.php
+++ templates/w3css/partials/entry_word_list.php
@@ -1,5 +1,5 @@
 <ul>
 <? foreach($list as $item): ?>
-    <li><span class="w3-tag w3-round w3-dark-grey"><?=$item;?></span></li>
+    <li><a class="w3-tag w3-round w3-dark-grey" href="../lexi/<?=$item;?>"><?=$item;?></a></li>
 <? endforeach; ?>
 </ul>
