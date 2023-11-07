7088fc795aab72a01fe9715f0f7b3bd9ea82b19c
shawn@snowbank.ca <shawn@snowbank.ca>
2023-11-07 14:54:54 +0000

    :bug: Fix HTML

--- templates/w3css/partials/page-footer.php
+++ templates/w3css/partials/page-footer.php
@@ -37,6 +37,3 @@ namespace WorldlangDict;
   <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
   <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.4.1.min.js"><\/script>')</script>
  -->
-</body>
-
-</html>
