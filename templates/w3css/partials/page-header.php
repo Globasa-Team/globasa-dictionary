6aca1965c92dcec3c83575130fc3a13ecb2211a2
shawn@snowbank.ca <shawn@snowbank.ca>
2023-11-28 19:51:27 +0000

    Style update

--- templates/w3css/partials/page-header.php
+++ templates/w3css/partials/page-header.php
@@ -18,10 +18,10 @@ namespace WorldlangDict;
     <form action="<?php echo WorldlangDictUtils::makeUri($config, "xerca", $request); ?>" method="get">
     <div class="w3-cell-row">
         <div class="w3-container w3-cell">
-            <input type="text" name="glb" placeholder="<?php echo $config->getTrans('search worldlang placeholder');?>" class="w3-input w3-border" value="" />
+            <input type="text" name="glb" placeholder="<?php echo $config->getTrans('search worldlang placeholder');?>" class="w3-input" value="" />
         </div>
         <div class="w3-container w3-cell">
-            <input type="text" name="<?=$request->lang; ?>" placeholder="<?php echo $config->getTrans('search natlang placeholder');?>" class="w3-input w3-border" value="" />
+            <input type="text" name="<?=$request->lang; ?>" placeholder="<?php echo $config->getTrans('search natlang placeholder');?>" class="w3-input" value="" />
         </div>
         <div class="w3-container w3-cell w3-cell-middle">
             <input type="submit" value="<?php echo $config->getTrans('search button');?>" class="w3-btn" />
