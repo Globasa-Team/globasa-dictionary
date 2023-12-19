7fb17573fdf7bd36d693f5feb322f88c65de4d49
shawn@snowbank.ca <shawn@snowbank.ca>
2023-12-19 18:45:33 +0000

    Update entry look

--- templates/w3css/partials/html-head.php
+++ templates/w3css/partials/html-head.php
@@ -7,15 +7,12 @@ namespace WorldlangDict;
   <meta name="description" content="<?php echo $page->description; ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <link rel="manifest" href="site.webmanifest">
   <link rel="apple-touch-icon" href="https://cdn.globasa.net/graphics/flower/globasa-flower.svg">
   <link rel="icon" type="image/png" href="https://cdn.globasa.net/graphics/flower/globasa-flower.svg">
-  <!-- Place favicon.ico in the root directory -->
-
   <link rel="stylesheet" href="<?php echo $config->templateUri; ?>css/normalize.css">
   <link rel="stylesheet" href="<?php echo $config->templateUri; ?>css/main.css">
-  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-  <link rel="stylesheet" href="<?php echo $config->templateUri; ?>css/globasa.css?w">
+  <!-- <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> -->
+  <link rel="stylesheet" href="<?php echo $config->templateUri; ?>css/globasa.css?x">
   <link href="https://fonts.googleapis.com/css?family=Literata|Merriweather&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fork-awesome@1.1.7/css/fork-awesome.min.css" integrity="sha256-gsmEoJAws/Kd3CjuOQzLie5Q3yshhvmo7YNtBG7aaEY=" crossorigin="anonymous">
   <script src="<?php echo $config->siteUri; ?>assets/ipa.js?02-08"></script>
