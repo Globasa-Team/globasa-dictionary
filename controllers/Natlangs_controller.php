221b947080a8aa02ac5263e0b85fde70b72226ae
shawn@snowbank.ca <shawn@snowbank.ca>
2024-01-18 18:36:30 +0000

    :bug: fix slash

--- controllers/Natlangs_controller.php
+++ controllers/Natlangs_controller.php
@@ -11,8 +11,8 @@ class Natlangs_controller
 {
     public static function run($config, $request, $page)
     {
-        $arg = isset($request->arguments[0]) ? $request->arguments[0] : '';
-
+        $arg = isset($request->arguments[0]) ? strtolower($request->arguments[0]) : '';
+        
         // TODO: i18n
         $page->description = "Natlangs sources for Globasa words";
         $page->title = "Natlangs";
@@ -23,9 +23,9 @@ class Natlangs_controller
                 require_once('views/natlangs_default_view.php');
                 break;
             default:
-                if (file_exists($config->api2Path.'etymologies\etymology_'.$arg.'.yaml')) {
-                    // todo: security on $arg
-                    $data = yaml_parse_file($config->api2Path.'etymologies\etymology_'.$arg.'.yaml');
+                if (file_exists($config->api2Path.'etymologies/etymology_'.$arg.'.yaml')) {
+                    // TODO: security on $arg
+                    $data = yaml_parse_file($config->api2Path.'etymologies/etymology_'.$arg.'.yaml');
                     $dict = yaml_parse_file($config->basic_location.$request->lang.'.yaml');
                     require_once('views/natlangs_language_view.php');
                 } else {
