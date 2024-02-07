2b04f75475c6f3059fe913df1afe8ae32f6cfa24
shawn@snowbank.ca <shawn@snowbank.ca>
2024-02-07 20:06:21 +0000

    Seperate changelog by date

--- views/reports_changelog_view.php
+++ views/reports_changelog_view.php
@@ -4,6 +4,8 @@ namespace WorldlangDict;
 
 // TODO: i18n
 
+$cur_date = "";
+
 ?>
 <!doctype html>
 <html class="no-js" lang="<?= $request->lang; ?>">
@@ -19,9 +21,19 @@ namespace WorldlangDict;
 <p>Updates to the word list.</p>
 
 <table>
-<? foreach($data as $cur) { ?>
+<? foreach($data as $cur) { 
+  if ($cur_date !== substr($cur['timestamp'], 0, 10)) {
+    $cur_date = substr($cur['timestamp'], 0, 10);
+  ?>
   <tr>
-    <td><strong><?= $cur['term']; ?></strong><br/><?= $cur['type']; ?><br/><?= substr($cur['timestamp'], 0, 10); ?></td>
+    <td span="2"><h2><?= substr($cur['timestamp'], 0, 10); ?></h2></td>
+    
+  </tr>
+  <?    
+  }
+  ?>
+  <tr>
+    <td><strong><?= $cur['term']; ?></strong><br/><?= $cur['type']; ?></td>
     <td><?= $cur['message']; ?></td>
   </tr>
 <? } ?>
