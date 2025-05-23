f8c4534c1866dad1ec654731d0f78b6b30843a5e
PartialShawn <shawn@snowbank.ca>
2025-05-23 15:06:38 +0000

    php update: use nullable type and move optional params

--- WorldlangDictUtils.php
+++ WorldlangDictUtils.php
@@ -3,7 +3,7 @@ namespace WorldlangDict;
 
 class WorldlangDictUtils
 {
-    public static function makeUri(WorldlangDictConfig $config, Request $request, string $controller, string|null $arg=null): string
+    public static function makeUri(WorldlangDictConfig $config, Request $request, string $controller, ?string $arg=null): string
     {
         if (isset($config->routes[$controller])) $controller = $config->routes[$controller];
         return $config->siteUri.$config->lang.'/'.$controller.(!empty($arg) ? '/'.$arg : '').$request->linkQuery;
@@ -24,13 +24,13 @@ class WorldlangDictUtils
             .$request->linkQuery;
     }
 
-    public static function redirect(WorldlangDictConfig $config, Request $request, string $controller="", string|null $arg=null): void
+    public static function redirect(WorldlangDictConfig $config, Request $request, string $controller="", ?string $arg=null): void
     {
         header('Location: '.WorldlangDictUtils::makeUri(config:$config, controller:$controller, arg:$arg, request:$request));
         exit(0);
     }
 
-    public static function makeLink(WorldlangDictConfig $config, string $controller, string|null $arg=null, Request $request, string|null $text=null): string
+    public static function makeLink(WorldlangDictConfig $config, Request $request, string $controller, ?string $arg=null, ?string $text=null): string
     {
         if ($text == null) $text = $controller;
         return '<a href="'.
