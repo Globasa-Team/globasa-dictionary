dd405dfe2b865a8b8b91afed2288e8c37eb198ec
PartialShawn <shawn@snowbank.ca>
2025-08-13 17:45:35 +0000

    Show translations of min-pair matches #39

--- controllers/Minimal_pair_controller.php
+++ controllers/Minimal_pair_controller.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 namespace WorldlangDict;
 
 class Minimal_pair_controller
@@ -6,8 +7,8 @@ class Minimal_pair_controller
 
     public static function check($config, $request, &$page)
     {
-        $index = yaml_parse_file($config->index_location);
-        $pairs = Minimal_pair::analyze($config, $request, $index);
+        $dict = yaml_parse_file($config->min_location."eng.yaml");
+        $pairs = Minimal_pair::analyze($config, $request, $dict);
         $page->setTitle($config->getTrans('minimum pair title'));
         $page->description = $config->getTrans('minimum pair description');
         $page->show_input = true;
