69a60f05364378db00598be52dd321302096d98e
PartialShawn <shawn@snowbank.ca>
2025-11-06 14:03:05 +0000

    remove unused funciton

--- controllers/Word_controller.php
+++ controllers/Word_controller.php
@@ -86,32 +86,6 @@ class Word_controller
     }
 
 
-    public static function getWordList($config, $request, $listLang, &$page)
-    {
-        $list = [];
-        if ($listLang == $config->worldlang) {
-            // NOTE Should this be in the Word class?
-            foreach ($config->dictionary->words as $word=>$wordData) {
-                $list[mb_strtolower($word, encoding:"UTF-8")] = new Word($config, $wordData);
-            }
-        } else {
-            // NOTE Should this be in the Word class?
-            foreach ($config->dictionary->index as $word=>$wordData) {
-                $wLWords = explode(',', $wordData);
-                if (sizeof($wLWords)==1) {
-                    $this->list[$word] = new Word($config, $config->dictionary[$config->worldlang][$wLWords[0]]);
-                } else {
-                    foreach ($wLWords as $wLWord) {
-                        $this->list[$word][$glbWord] = new Word($config, $config->dictionary[$config->worldlang][mb_trim($wLWord, encoding:"UTF-8")]);
-                    }
-                }
-            }
-        }
-
-        $page->setTitle($config->getTrans('all words button'));
-        WordListView::addList($config, $request, $list, $page);
-    }
-
     public static function randomWord($config, $request, &$page)
     {
         $index = yaml_parse_file($config->min_location.$config->lang.".yaml");
